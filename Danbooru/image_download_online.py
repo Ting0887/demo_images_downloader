293b14df41e1bbf6b65fc1deecee076debaf61fd
Ting0887 <57065036+Ting0887@users.noreply.github.com>
2023-09-08 22:41:25 +0800

    fixed bug

--- Danbooru/image_download_online.py
+++ Danbooru/image_download_online.py
@@ -1,6 +1,7 @@
 from threading import local
 from flask import Flask, render_template, request
 from scraper import Danbooru
+from scraper import url_encode
 import time
 
 app = Flask('__name__', 
@@ -13,20 +14,21 @@ def download():
     if request.method == 'POST':
         keyword = str(request.form.get('keyword'))
         
-        # if keyword have _
+        # if keyword only have _
         if " " in keyword:
             split_kws = keyword.split(" ")
-            keyword = split_kws[0] + "+" + split_kws[1]
-        print(keyword)        
+            final_keyword = split_kws[0] + "+" + split_kws[1]
+        else:
+            final_keyword = keyword
         try:
             num_page = int(request.form.get('num_page'))
         except Exception as e:
             num_page = request.form.get('num_page')
-            print(e)
+        
         folder_name = str(request.form.get('folderpath'))
         start = time.time()
         
-        danb = Danbooru(keyword, num_page, folder_name)
+        danb = Danbooru(final_keyword, num_page, folder_name)
         
         # check image can be found
         Notfound_err = danb.check_notfound()
